45257343b87b0c3cb0dc4a5c549f9c7072592d7f
deffohn <maxime13.p@gmail.com>
2023-01-04 09:54:22 +0100

    full-merge

--- _modules/vpc/main.tf
+++ _modules/vpc/main.tf
@@ -1,3 +1,8 @@
-resource "aws_vpc" "demo" {
+resource "aws_vpc" "main_vpc" {
   cidr_block = var.cfg.vpc.cidr
+
+  # TODO tags
+  tags = {
+    Name = "main_vpc"
+  }
 }
--- production/main.tf
+++ production/main.tf
@@ -1,16 +1,5 @@
-module VPC {
-    source = "./VPC"
-    subnets_azs = var.subnets_azs
-}
-
-module compute {
-    source = "./compute"
-    vpc_id = module.VPC.vpc_id
-    public_subnets_ids = module.VPC.public_subnets_ids
-    private_subnets_ids = module.VPC.private_subnets_ids
-}
-
-module cloudwatch {
-    source = "./cloudwatch"
-    private_instances_ids = module.compute.instances_ids
+locals { cfg = yamldecode(file("../config/production.yml")) }
+module vpc {
+    source = "../_modules/vpc"
+    cfg = local.cfg
 }
